a01a44282d2f231ba7d82568ce88d1601d1ddf4e
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-04-02 11:02:10 +0300

    Return first item from queryItem

--- packages/server/src/providers/DAO.js
+++ packages/server/src/providers/DAO.js
@@ -4,13 +4,13 @@ export const createDAO = appCtx => {
   const { db } = appCtx;
   const DAO = {
     async queryItem(id) {
-      return camelizeKeys(await db('item').select().where({ id }));
+      return camelizeKeys(await db('item').select().where({ id }).first());
     },
     async queryItems() {
       return camelizeKeys(await db('item').select());
     },
     async createItem(title, description) {
-      const [ id ] = await db('item').insert({ title, description });
+      const [id] = await db('item').insert({ title, description });
       return this.queryItem(id);
     }
   };
