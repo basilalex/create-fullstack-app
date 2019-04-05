9bd966a3daa9971b28dd9445801f1677145643a5
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-04-05 15:44:41 +0300

    Fix typo

--- packages/server/src/utils.js
+++ packages/server/src/utils.js
@@ -3,4 +3,4 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 export const pipe = (...fns) => arg => fns.reduce((v, fn) => fn(v), arg);
-export const getEnv = () => dotenv.config({ path: path.join(process.cwd() + '/.env.development')}).parsed;
+export const getEnv = () => dotenv.config({ path: path.join(process.cwd() + '/.env')}).parsed;
